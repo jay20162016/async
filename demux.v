7fa7b85c39a54e4bc02c4fa456c0a52b63ae2f18
Jay Jayjay <jayjayjay@example.fios-router.home>
2021-02-20 11:21:50 -0500

    demux! not tested :(

---
+++ demux.v
@@ -0,0 +1,52 @@
+`ifndef __demux
+
+`include "mullerc.v"
+
+module demux #(
+    parameter Rpol = 1'b0, // reset polarity (rst=rpol => reset)
+
+    parameter N = 32'b1,
+
+    parameter NATIVE = 1'b1 // don't optimize
+  ) (
+  input r_i,
+  output a_i,
+  input [N-1:0] d_i,
+
+  input ctl_a,
+  input ctl_b,
+  output actl_i,
+
+  output r_o,
+  input a_o,
+  output [N-1:0] d_o,
+
+  output r1_o,
+  input a1_o,
+  output [N-1:0] d1_o,
+
+  input rst);
+
+  assign d_o = d_i;
+  assign d1_o = d_i;
+
+  wire acki_i;
+  assign acki_i = a_o | a1_o;
+
+  assign actl_i = acki_i;
+  assign a_i = acki_i;
+
+  mullerc #(.Rval(1'b0), .Rpol(Rpol), .NATIVE(NATIVE)) r0 (
+    ctl_a, r_i,
+    r_o,
+    rst);
+
+  mullerc #(.Rval(1'b0), .Rpol(Rpol), .NATIVE(NATIVE)) r1 (
+    ctl_b, r_i,
+    r1_o,
+    rst);
+
+endmodule
+
+`define __demux
+`endif
---
+++ demux2.v
@@ -0,0 +1,41 @@
+`ifndef __demux2
+
+`include "demux2.v"
+
+module demux2 #(
+    parameter Rpol = 1'b0, // reset polarity (rst=rpol => reset)
+
+    parameter N = 32'b1,
+
+    parameter NATIVE = 1'b1 // don't optimize
+  ) (
+  input r_i,
+  output a_i,
+  input [N-1:0] d_i,
+
+  input rctl_i,
+  input dctl_i,
+  output actl_i,
+
+  output r_o,
+  input a_o,
+  output [N-1:0] d_o,
+
+  output r1_o,
+  input a1_o,
+  output [N-1:0] d1_o,
+
+  input rst);
+
+  demux #(.Rpol(Rpol), .N(N), .NATIVE(NATIVE)) main (
+    r_i, a_i, d_i,
+    rctl_i & ~dctl_i, rctl_i & dctl_i, actl_i,
+    r_o, a_o, d_o,
+    r1_o, a1_o, d1_o,
+    rst
+    );
+
+endmodule
+
+`define __demux2
+`endif
